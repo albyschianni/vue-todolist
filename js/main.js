949355d52ae7bce69e6a40601a539c23c7d74a21
albyschianni <alberto.schianni@gmail.com>
2022-11-29 17:45:37 +0100

    final v.1

--- js/main.js
+++ js/main.js
@@ -8,12 +8,29 @@ const { createApp } = Vue
 createApp ({
     data (){
         return {
+            error: false,
             message: "",
+            newTask: "",
             tasks: [
                 { text: 'Fare i compiti', done: false }, 
                 { text: 'Fare la spesa', done: true }, 
                 { text: 'Fare il bucato', done: false }
                 ]
+        }
+    },
+    methods: {
+        addTask(){
+            if (this.newTask.length < 5 || this.newTask === ''){
+                this.error = true;
+            } else {
+                this.tasks.unshift({text:this.newTask});
+                this.error = false;
+            }
+            
+            this.newTask = "";
+        },
+        deleteTask(indiceArgomento){
+            this.tasks.splice(indiceArgomento, 1);
         }
     }
 }).mount("#app");
